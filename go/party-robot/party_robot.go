e9e83469159bd10785ec6180ac9d0ef83e5a1f0d
Carl Parrish <carl.parrish@gmail.com>
2025-03-12 11:37:22 -0700

    feat: add package and fmt exercise

---
+++ go/party-robot/party_robot.go
@@ -0,0 +1,24 @@
+package partyrobot
+
+import "fmt"
+
+// Welcome greets a person by name.
+func Welcome(name string) string {
+	msg := fmt.Sprintf("Welcome to my party, %s!", name)
+	return msg
+}
+
+// HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
+func HappyBirthday(name string, age int) string {
+	msg := fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+	return msg
+}
+
+// AssignTable assigns a table to each guest.
+func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
+	welcome := Welcome(name)
+	companion := fmt.Sprintf("You will be sitting next to %s", neighbor)
+	assignment := fmt.Sprintf("You have been assigned to table %.3d. Your table is %s, exactly %.1f meters from here.", table, direction, distance)
+	return welcome + "\n" + assignment + "\n" + companion + "."
+
+}
